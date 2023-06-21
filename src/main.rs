a81595394e57c0651f8c3ee481f3bc53f2747802
Sam Peters <sam.peters24@gmail.com>
2023-06-21 21:43:22 +0100

    refactoring and states

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, window::WindowResized, math::{vec3, vec2}, diagnostic::Diagnostics};
+use bevy::{prelude::*, window::WindowResized, math::{vec3, vec2}, diagnostic::Diagnostics, input::mouse};
 
 const BACKGROUND_COLOR:Color = Color::rgb(0.15, 0.15, 0.15);
 
@@ -23,23 +23,55 @@ struct WindowSize {
     height:f32
 }
 
+#[derive(Resource)]
+struct MousePosition {
+    x:f32,
+    y:f32,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default, States)]
+enum GameState {
+    MainMenu,
+    SettingsMenu,
+    #[default]
+    InGame,
+}
+
 fn main() {
     App::new()
+        .add_state::<GameState>()
         .insert_resource(ClearColor( BACKGROUND_COLOR ))
         .insert_resource(WindowSize{width:100.0,height:100.0})
+        .insert_resource(MousePosition{x:0.0,y:0.0})
 
         .add_plugins(DefaultPlugins)
         .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin::default())
 
         .add_startup_system(setup_window)
         .add_startup_system(setup_entities)
-        .add_startup_system(setup_ui)
-        .add_system(window_resized)
-        .add_system(player_movement)
-        .add_system(aim_at_mouse)
-        .add_system(firing_test)
-        .add_system(draw_ray)
+        .add_startup_system(setup_fps_counter)
+
+        .add_system(mouse_pos_updater)
+        .add_system(window_resize_handler)
         .add_system(update_fps_counter)
+
+        // Ingame systems
+        .add_systems((
+            player_movement,
+            aim_at_mouse,
+            firing_test,
+            draw_ray
+        ).distributive_run_if(in_state(GameState::InGame)))
+            
+        // // Main Menu systems (when you quit or start the game)
+        // .add_systems((
+            
+        // ).distributive_run_if(in_state(GameState::MainMenu)))
+
+        // // Settings systems (when you press escape)
+        // .add_systems((
+
+        // ).distributive_run_if(in_state(GameState::SettingsMenu)))
 
         .run();
 }
@@ -51,6 +83,7 @@ fn setup_entities(
 ) {
     commands.spawn(Camera2dBundle::default());
 
+    // cursor
     let ball_radius = 8.0;
     let ball_mesh:Mesh = shape::Circle::new(ball_radius).into();
     let ball_material = ColorMaterial::from(Color::rgb(100.,100.,100.));
@@ -66,6 +99,7 @@ fn setup_entities(
         MouseCursorObj,
     ));
 
+    // Player triangle
     let triangle_mesh:Mesh = shape::RegularPolygon::new(20.0, 3).into();
     let ball_material = ColorMaterial::from(Color::rgb(100.,100.,100.));
     let ball_pos: Vec3 = Vec3::new(0.0, 0.0, 0.0);
@@ -80,10 +114,7 @@ fn setup_entities(
         PlayerObj,
     ));
 
-
-
     // Test Collision object
-    let quad_radius = 8.0;
     let quad_mesh:Mesh = shape::Quad::new(vec2(50.0, 20.0)).into();
     let quad_material = ColorMaterial::from(Color::rgb(100.,100.,100.));
     let quad_pos: Vec3 = Vec3::new(100.0, 200.0, 0.0);
@@ -99,8 +130,7 @@ fn setup_entities(
     ));
 
 
-    // testing ray object
-    let quad_radius = 8.0;
+    // draw ray object
     let quad_mesh:Mesh = shape::Quad::new(vec2(1.0, 1.0)).into();
     let quad_material = ColorMaterial::from(Color::rgb(100.,100.,100.));
     let quad_pos: Vec3 = Vec3::new(0.0, 0.0, 0.0);
@@ -114,39 +144,33 @@ fn setup_entities(
         },
         RayObj
     ));
-    
-
 
 }
 
-fn mouse_pos_res() {
-    
+fn mouse_pos_updater(
+    mut mouse: EventReader<CursorMoved>,
+    mut mouse_pos: ResMut<MousePosition>,
+    window_size: Res<WindowSize>,
+) {
+    for ev in mouse.iter() {
+        mouse_pos.x = ev.position.x - window_size.width/2.0;
+        mouse_pos.y = ev.position.y - window_size.height/2.0;
+    }
 }
 
 fn aim_at_mouse (
-    mut mouse: EventReader<CursorMoved>,
-    mut look_at_mouse: Query<&mut Transform, (With<PlayerObj>, Without<MouseCursorObj>)>,
-    mut cursor_obj: Query<&mut Transform, (With<MouseCursorObj>, Without<PlayerObj>)>,
-    window_size: Res<WindowSize>,
+    mut look_at_mouse: Query<&mut Transform, With<PlayerObj>>,
+    mouse_pos: Res<MousePosition>,
 ) {
-    let mut cursor_obj = cursor_obj.get_single_mut().expect("cursor Object brokie");
     let mut player = look_at_mouse.get_single_mut().expect("Player obj brokie");
-
-    for ev in mouse.iter() {
-
-        cursor_obj.translation.x = ev.position.x - window_size.width/2.0;
-        cursor_obj.translation.y = ev.position.y - window_size.height/2.0;
-
-    }
 
     let x = player.translation.x;
     let y = player.translation.y;
 
     player.look_at(
         vec3(x, y, 999.9),
-        vec3(cursor_obj.translation.x-x, cursor_obj.translation.y-y, 0.0)
+        vec3(mouse_pos.x-x, mouse_pos.y-y, 0.0)
     );
-
 }
 
 fn player_movement(
@@ -187,7 +211,6 @@ fn player_movement(
 
     // Cba right now it doesn't matter as much as getting normal functionality going like shooting and stuff (do it if i can't be bothered with other shit)
     // I also just realised i don't have an actual plan for what this game is about lmao, just mindlessly making games for some fucking reason
-
     
 }
 
@@ -219,28 +242,30 @@ fn firing_test(
     }
 }
 
-
-
 fn draw_ray(
     player: Query<&Transform, With<PlayerObj>>,
-    cursor_obj: Query<&Transform, With<MouseCursorObj>>,
-    mut ray_object: Query<&mut Transform, (With<RayObj>, Without<PlayerObj>, Without<MouseCursorObj>)>,
+    mouse_pos: Res<MousePosition>,
+    mut ray_object: Query<&mut Transform, (With<RayObj>, Without<PlayerObj>)>,
 ) {
+    // Sets the ray_obj.translation to the midpoint between the mouse and player
+    // then sets the length of the ray_obj to the distance between the player and the cursor
+    // finally it makes the ray_obj point towards the mouse position (look_at())
+    // this has the effect of drawing a line between the player and the cursor
+
+    // Might want to turn this into a plugin or something, twas an absolute pain to make
     let player = player.single();
-    let cursor = cursor_obj.single();
     let mut ray_obj = ray_object.single_mut();
     
-    
-    let ct = cursor.translation;
-    let rt = ray_obj.translation;
+    let mouse_vec = vec3(mouse_pos.x, mouse_pos.y, 0.0);
+    ray_obj.translation = ((mouse_vec - player.translation)/2.0)+player.translation;
+    ray_obj.scale.y = player.translation.distance(mouse_vec);
+
+    let rt = ray_obj.translation; // Ray-Translation(rt) local store operation
     ray_obj.look_at(
         vec3(rt.x, rt.y, 999.9),
-        vec3(ct.x-rt.x, ct.y-rt.y, 0.0)
+        vec3(mouse_pos.x-rt.x, mouse_pos.y-rt.y, 0.0)
     );
-    ray_obj.translation = ((cursor.translation-player.translation)/2.0)+player.translation;
-    ray_obj.scale.y = player.translation.distance(cursor.translation);
 }
-
 
 // Should probably move the fps counter functions to a plugin or whatever
 fn update_fps_counter(
@@ -250,11 +275,10 @@ fn update_fps_counter(
     // MY EYES
     let fps = diag.get(bevy::diagnostic::FrameTimeDiagnosticsPlugin::FPS).and_then(|fps| fps.average()).unwrap_or(0.0);
     let mut fps_counter = fps_counter_qur.get_single_mut().expect("fps counter doesn't exist");
+    // the "{:.5}" is the number of decimal places to display ("{:.3}" would display 3 decimal places)
     fps_counter.sections[0].value = format!("fps:{:.5}",fps).to_string();
 }
-
-
-fn setup_ui(
+fn setup_fps_counter(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     window: Res<WindowSize>
@@ -280,20 +304,17 @@ fn setup_ui(
     ));
 }
 
-
 fn setup_window(
     mut resize_event: EventReader<WindowResized>,
     mut window_size: ResMut<WindowSize>,
 ) {
-    // let window = resize_event.iter().nth(0).expect("Window Setup Failed");
     for i in resize_event.iter() {
         window_size.width = i.width;
         window_size.height = i.height;
     }
 }
 
-
-fn window_resized(
+fn window_resize_handler(
     mut resize_event: EventReader<WindowResized>,
     mut window_size: ResMut<WindowSize>,
 ) {
